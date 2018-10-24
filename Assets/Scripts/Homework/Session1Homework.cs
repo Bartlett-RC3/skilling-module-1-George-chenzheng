0bda65a6c892270d936424fb51f277c678b9257b
George-chenzheng <44257844+George-chenzheng@users.noreply.github.com>
2018-10-24 22:48:21 +0100

    Update Assets/Scripts/Homework/Session1Homework.cs
    I has written the code about variables ,
    array , list and dictionary . please check whether my code is right .

--- Assets/Scripts/Homework/Session1Homework.cs
+++ Assets/Scripts/Homework/Session1Homework.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Session1Homework : MonoBehaviour {
+public class Session1Homework : MonoBehaviour
+
+{
+
+    public int myIntNumber= 50;
+    public float myFloatNumber= 30.5f;
+    public string myString = "My tutors is intelligent and handsome";
+
+    public int[] myArray = new int[6] { 1,2,3,4,5,6 };
+    public List<int> myIntList = new List<int>(5);
+
+    Dictionary<string, string> country = new Dictionary<string, string>();
+
 
 	// Use this for initialization
-	void Start () {
+	public void Start ()
+    {
+        Debug.Log(myArray[2].ToString());
+
+
+        myIntList.Add(4321);
+        myIntList.Add(41);
+        myIntList.Add(1);
+
+        Debug.Log(myIntList[2].ToString());
+        Debug.Log(myIntList[myIntList.Count - 1].ToString());
+
+        country.Add("Asian", "China");
+        country.Add("Europe", "U.K.");
+        country.Add("America", "Canada");
+        country.Add("Africa", "Congo");
+        country.Add("Oceania", "Australia");
+
+        if (country.ContainsKey("asian"))
+        {
+            Debug.Log("A Asian country is " + country.Values);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void PrintSomeNumbers()
+    {
+        Debug.Log(myIntNumber);
+        Debug.Log(myFloatNumber);
+        Debug.Log(myString);
+    }
+
+
 }
