7167e17c49e4bc1a30fe7f6d7b5261d6e1ba6a45
George-chenzheng <44257844+George-chenzheng@users.noreply.github.com>
2018-10-24 22:50:45 +0100

    Update Assets/Scripts/Homework/Session1Homework.cs

--- Assets/Scripts/Homework/Session1Homework.cs
+++ Assets/Scripts/Homework/Session1Homework.cs
@@ -8,7 +8,7 @@ public class Session1Homework : MonoBehaviour
 
     public int myIntNumber= 50;
     public float myFloatNumber= 30.5f;
-    public string myString = "My tutors is intelligent and handsome";
+    public string myString = "My tutor is intelligent and handsome";
 
     public int[] myArray = new int[6] { 1,2,3,4,5,6 };
     public List<int> myIntList = new List<int>();
